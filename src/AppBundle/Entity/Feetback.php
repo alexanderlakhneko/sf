4f08fd8f3a6913aaf0a6fe3f74a5f657936d2d1a
alexanderlakhneko <alexanderlakhneko@gmail.com>
2017-05-27 13:43:06 +0300

    part 1

---
+++ src/AppBundle/Entity/Feetback.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Feetback
+ *
+ * @ORM\Table(name="feetback")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FeetbackRepository")
+ */
+class Feetback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="massage", type="text")
+     */
+    private $massage;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ip_adress", type="string", length=50)
+     */
+    private $ipAdress;
+
+    /**
+     * @return string
+     */
+    public function getIpAdress()
+    {
+        return $this->ipAdress;
+    }
+
+    /**
+     * @param string $ipAdress
+     */
+    public function setIpAdress($ipAdress)
+    {
+        $this->ipAdress = $ipAdress;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Feetback
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Feetback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set massage
+     *
+     * @param string $massage
+     *
+     * @return Feetback
+     */
+    public function setMassage($massage)
+    {
+        $this->massage = $massage;
+
+        return $this;
+    }
+
+    /**
+     * Get massage
+     *
+     * @return string
+     */
+    public function getMassage()
+    {
+        return $this->massage;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return Feetback
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
+
